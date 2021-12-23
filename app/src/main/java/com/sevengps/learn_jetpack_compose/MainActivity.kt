3f5b7de6538949e94fd4530982a2722cbce160d5
ndeme yvan <yvan.ndeme@papersoft.ng>
2021-12-23 16:35:39 +0100

    commit

---
+++ app/src/main/java/com/sevengps/learn_jetpack_compose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.sevengps.learn_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sevengps.learn_jetpack_compose.ui.theme.Learn_jetpack_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Learn_jetpack_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Learn_jetpack_composeTheme {
+        Greeting("Android")
+    }
+}

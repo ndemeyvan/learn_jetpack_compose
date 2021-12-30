7aaf5b43f6da542ced795597b7cb61ffd6e9ce85
ndemeyvan <ndemeyvan>
2021-12-30 11:22:34 +0100

    learn box in jetpack compose

--- app/src/main/java/com/sevengps/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/sevengps/learn_jetpack_compose/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.sevengps.learn_jetpack_compose
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sevengps.learn_jetpack_compose.ui.theme.Learn_jetpack_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,14 +23,7 @@ class MainActivity : ComponentActivity() {
             Learn_jetpack_composeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalArrangement = Arrangement.SpaceAround,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Item(weight = 3f, color = MaterialTheme.colors.secondary)
-                        Item(weight = 1f, color = MaterialTheme.colors.primary)
-                    }
+                    Screen()
                 }
             }
         }
@@ -37,18 +31,27 @@ class MainActivity : ComponentActivity() {
 }
 
 
-///Certaine propriete de colonne pour
-// etre utiliser doivent etre dans le scope de la colonne
-//D'ou l'importance d'ajouter un ColumnScope/RowScope
 @Composable
-fun RowScope.Item(weight: Float, color: androidx.compose.ui.graphics.Color) {
-    Surface(
-        modifier = Modifier
-            .width(50.dp)
-            .height(50.dp)
-            .weight(weight),
-        color = color
-    ) {}
+fun Screen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = androidx.compose.ui.graphics.Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(50.dp)
+                    .background(androidx.compose.ui.graphics.Color.Green)
+            )
+            Text(text = "I Love Android ", fontSize = 40.sp)
+        }
+    }
 }
 
 
@@ -56,15 +59,7 @@ fun RowScope.Item(weight: Float, color: androidx.compose.ui.graphics.Color) {
 @Composable
 fun DefaultPreview() {
     Learn_jetpack_composeTheme {
-        //Le modifier Modifier.fillMaxSize() demande de remplir l'ecran
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Item(weight = 3f, color = MaterialTheme.colors.secondary)
-            Item(weight = 1f, color = MaterialTheme.colors.primary)
-        }
+        Screen()
 
     }
 }

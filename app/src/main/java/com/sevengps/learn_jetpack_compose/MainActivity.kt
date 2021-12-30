320c4564b341c2b1b217c2031dddfc3086cc5f38
ndemeyvan <ndemeyvan>
2021-12-30 09:59:40 +0100

    learn about collum and row with jetpack compose android natif

--- app/src/main/java/com/sevengps/learn_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/sevengps/learn_jetpack_compose/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.sevengps.learn_jetpack_compose
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sevengps.learn_jetpack_compose.ui.theme.Learn_jetpack_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +22,49 @@ class MainActivity : ComponentActivity() {
             Learn_jetpack_composeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Item(weight = 3f, color = MaterialTheme.colors.secondary)
+                        Item(weight = 1f, color = MaterialTheme.colors.primary)
+                    }
                 }
             }
         }
     }
 }
 
+
+///Certaine propriete de colonne pour
+// etre utiliser doivent etre dans le scope de la colonne
+//D'ou l'importance d'ajouter un ColumnScope/RowScope
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RowScope.Item(weight: Float, color: androidx.compose.ui.graphics.Color) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Learn_jetpack_composeTheme {
-        Greeting("Android")
+        //Le modifier Modifier.fillMaxSize() demande de remplir l'ecran
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Item(weight = 3f, color = MaterialTheme.colors.secondary)
+            Item(weight = 1f, color = MaterialTheme.colors.primary)
+        }
+
     }
 }
